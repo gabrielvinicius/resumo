177269f2d10d6a4f0c8624a76534f02cb68b2f7e
Gabriel Vinicius <gabrielvinicius.sds@gmail.com>
2024-03-05 12:41:03 -0300

    commit 05/03

--- app/transcription.py
+++ app/transcription.py
@@ -3,6 +3,7 @@ import whisper
 from moviepy.editor import VideoFileClip
 from app.models import db, Video
 import numpy as np  # Importe a biblioteca NumPy
+import os
 
 def transcribe_video_audio(video_id):
     # Recupera o vídeo do banco de dados
@@ -14,19 +15,22 @@ def transcribe_video_audio(video_id):
     # Carrega o vídeo usando MoviePy
     video_clip = VideoFileClip(video.video_path)
 
-    # Obtém o áudio do vídeo
-    audio = video_clip.audio.to_soundarray()  # Converte o áudio para um array NumPy
 
-    # Certifique-se de que o áudio seja um array NumPy
-    if not isinstance(audio, np.ndarray):
-        raise TypeError("Audio must be a NumPy array")
+    # Salva o áudio do vídeo em um arquivo wav
+    # Obtém o nome do arquivo do vídeo sem a extensão
+    video_name = os.path.splitext(os.path.basename(video.video_path))[0]
+    # Define o caminho do arquivo wav usando o mesmo nome do vídeo
+    audio_path = os.path.join(os.path.dirname(video.video_path), video_name + ".wav")
+    if not os.path.exists(audio_path):
+        # Escreve o áudio em um arquivo wav usando o MoviePy
+        video_clip.audio.write_audiofile(audio_path)
 
     model = whisper.load_model("base")
     # Realiza a transcrição usando Whisper
-    transcription = model.transcribe(audio)
+    transcription = model.transcribe(audio_path)
 
     # Salva a transcrição no banco de dados
-    video.transcription = transcription
+    video.transcription = transcription['text']
     db.session.commit()
 
     return transcription
