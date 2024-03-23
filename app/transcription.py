cc9180e3b7f0e325e3aaacc3dab0535b7aeb597c
Gabriel Vinicius <gabrielvinicius.sds@gmail.com>
2024-03-23 10:56:18 -0300

    commit 23/03 -  casa

--- app/transcription.py
+++ app/transcription.py
@@ -7,7 +7,7 @@ from moviepy.editor import VideoFileClip
 
 
 class SpeechTranscriber:
-    def __init__(self, model_name='large-v3'):
+    def __init__(self, model_name='base'):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
         self.model = whisper.load_model(name=model_name)
