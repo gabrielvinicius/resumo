7ec7984c9c467050e89d654aab0c2f748fa5c1e2
Gabriel Vinicius <gabrielvinicius.sds@gmail.com>
2024-04-03 22:23:31 -0300

    commit 03/04 -  casa

--- app/faster_whisper.py
+++ app/faster_whisper.py
@@ -12,7 +12,7 @@ class SpeechTranscriber:
     def __init__(self, model_size="base"):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.torch_dtype = 'float16' if torch.cuda.is_available() else 'float32'
-        self.model = WhisperModel(model_size, num_workers=multiprocessing.cpu_count() * 2 + 1)
+        self.model = WhisperModel(model_size, device=self.device, num_workers=multiprocessing.cpu_count() * 2 + 1)
 
     def transcribe(self, audio_path):
         start_time = time.time()
